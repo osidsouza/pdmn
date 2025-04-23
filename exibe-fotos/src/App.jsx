f392af88f0624b0bc123b4689c3d52df5890abe9
osidsouza <s.p.souza@hotmail.com>
2025-04-23 00:43:32 -0300

    learn(ap07-react): encerra a secao 2.2

---
+++ exibe-fotos/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Exibir uma lista de...</h1>
+      <i className="pi pi-search"></i>
+      
+    </div>
+  )
+}
+
+export default App
