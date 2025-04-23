1be988adc826270d8a51e46aab3f3a01c2ae90a7
osidsouza <s.p.souza@hotmail.com>
2025-04-23 01:55:27 -0300

    learn(ap07-react):  encerra a secap 2.6

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import Busca from './components/Busca'
 
-const App = () => {
-  return (
-    <div>
-      <h1>Exibir uma lista de...</h1>
-      <i className="pi pi-search"></i>
-      
-    </div>
-  )
+class App extends React.Component{
+
+  onBuscaRealizada = (termo) => {
+    console.log(termo)
+  }
+  render() {
+    return (
+      <div
+        className='grid justify-content-center'>
+        <div className='col-12'>
+          <h1>Exibir uma lista de...</h1>
+        </div>
+        <div className='col-12'>
+          <Busca 
+            dica='Buscar Imagens de...' 
+            onBuscaRealizada={this.onBuscaRealizada}/>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
